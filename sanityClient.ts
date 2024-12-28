56db0c64a637fe6c5423db90e007cf56e003d66a
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-12-28 16:40:25 +0500

    Update sanityClient.ts

--- sanityClient.ts
+++ sanityClient.ts
@@ -2,9 +2,9 @@ import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 const client = createClient({
-  projectId: '5hig8a62', 
-  dataset: 'production',  
-  apiVersion: '2024-11-15',  
+  projectId: process.env.NEXT_PUBLIC_SANITY_ID, 
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,  
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,  
   useCdn: true,
 });
 
